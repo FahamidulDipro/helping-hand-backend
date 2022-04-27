2fb1f82f0f658ad790bab3190d4baa26a116c700
FahamidulDipro <mdfahamidulislam047@gmail.com>
2022-04-27 14:00:54 +0600

    addService and delete selected service added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 //Middlewares
 app.use(cors());
@@ -40,12 +40,38 @@ async function run() {
     });
 
     //POST id
+    const selectedServiceCollection = client
+      .db("helping-hands-db")
+      .collection("events");
+    //Adding New Volunteer Service
+    app.post("/addServices", async (req, res) => {
+      const addedService = req.body;
+      const result = await serviceCollection.insertOne(addedService);
+      res.send(result);
+    });
     app.post("/services", async (req, res) => {
       const selectedService = req.body;
-      const selectedServiceCollection = client
-        .db("helping-hands-db")
-        .collection("events");
+
       const result = await selectedServiceCollection.insertOne(selectedService);
+      res.send(result);
+    });
+
+    //Get the events
+    app.get("/selected-services", async (req, res) => {
+      const query = {};
+      const cursor = selectedServiceCollection.find(query);
+      const selectedServices = await cursor.toArray();
+      res.send(selectedServices);
+    });
+
+    //Delete any event
+    app.delete("/delete/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = { _id: ObjectId(id) };
+      console.log(query);
+      const result = await selectedServiceCollection.deleteOne(query);
+
       res.send(result);
     });
   } finally {
